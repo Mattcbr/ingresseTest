10b179c8a2972ef6cb6bdd12ef534cabb2611cd8
mattcbr <matheus.castelo@gmail.com>
2018-05-10 09:30:41 -0300

    Starting the project

---
+++ Pods/Target Support Files/Pods-ingresseTest/Pods-ingresseTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ingresseTest : NSObject
+@end
+@implementation PodsDummy_Pods_ingresseTest
+@end
---
+++ ingresseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ingresseTest
+//
+//  Created by Matheus Queiroz on 5/9/18.
+//  Copyright © 2018 mattcbr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
